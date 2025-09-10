de88dab6c782a4fddfe1f338c02c80607115a534
tithoft <99927615+tithoft@users.noreply.github.com>
2025-09-10 13:57:56 -0400

    Script added to create grid and listener for mouse
    over grid to change color.

---
+++ etch.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    square.addEventListener("mouseover", () => {square.style.backgroundColor = "blue"});
+}
